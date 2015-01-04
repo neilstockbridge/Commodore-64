2ce5881d59ab713d9a4dd072d9eec7b441bbea48
Neil Stockbridge <neil@dist.ro>
2015-01-05 10:05:21 +1300

    the software sprite-to-character collision example is now clearer and includes wall and ceiling collision indicators

--- examples/software-sprite-to-char-collision/main.c
+++ examples/software-sprite-to-char-collision/main.c
@@ -1,19 +1,32 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <c64.h>
 #include <string.h> // for memset
-#include <conio.h>
+#include <c64.h>
+#include <conio.h>  // for cput*
 
 
 #define  CHAR_MATRIX      ((uint8_t*) 0x0400)
 #define  SHAPE_FOR_SPRITE ((uint8_t*) 0x07f8)
 #define  TEST_SHAPE       ((uint8_t*) 0x4000 - 64)
 
+#define  SPRITE_WIDTH   24
+#define  SPRITE_HEIGHT  21
 
-void paint( row, column, code )
+// This is the lowest Sprite Y ordinate for which the top row of pixels of the
+// sprite are still visible
+#define  SPRITE_Y_TOP   50
+#define  SPRITE_X_LEFT  24
+
+
+void place( code, column, row)
 {
   CHAR_MATRIX[ 40* row+ column] = code;
+}
+
+void paint( color, column, row )
+{
+  COLOR_RAM[ 40*row + column] = color;
 }
 
 
@@ -53,64 +66,99 @@ void move_sprite_on_x( int8_t amount )
 }
 
 
-uint8_t  cell_x;
-uint8_t  cell_y;
-
-
 void render()
 {
   uint16_t  pixel_x;
   uint8_t   pixel_y;
-  uint8_t   new_cell_x;
-  uint8_t   new_cell_y;
+  uint8_t   center_column;
+  uint8_t   row_above;
+  uint8_t   row_below;
 
   // Work out the pixel-granularity co-ordinates within the character matrix
-  // ( rather than the visible display) of the pixel immediately to the
-  // lower-right of the sprite
-  #define  SPRITE_WIDTH  24
-  #define  SPRITE_HEIGHT  21
+  // ( rather than the visible display) of the pixel in the top-left hand
+  // corner of the sprite
   #define  FINE_SCRL_Y  ( VIC.ctrl1 & 0x7 )
   #define  FINE_SCRL_X  ( VIC.ctrl2 & 0x7 )
-  #define  NORM_FINE_Y  3  // The value of FINE_SCRL_Y 
-  pixel_x = ( ((VIC.spr_hi_x >> 0 & 0x1)<< 8) + VIC.spr0_x) - 24 + SPRITE_WIDTH - FINE_SCRL_X;
-  pixel_y = VIC.spr0_y - 50 + SPRITE_HEIGHT + NORM_FINE_Y - FINE_SCRL_Y;
+  #define  NORM_FINE_Y  3
+  pixel_x = ( ((VIC.spr_hi_x >> 0 & 0x1)<< 8) + VIC.spr0_x) - SPRITE_X_LEFT - FINE_SCRL_X;
+  pixel_y = VIC.spr0_y - SPRITE_Y_TOP + NORM_FINE_Y - FINE_SCRL_Y;
 
   // Show the parameters to the formula
   cputsxy( 1, 21, "fine: $"); cputhex8( FINE_SCRL_X); cputc(','); cputhex8( FINE_SCRL_Y);
   cputsxy( 1, 22, "sprt: $"); cputhex8( VIC.spr0_x); cputc(','); cputhex8( VIC.spr0_y);
   cputsxy( 1, 23, "cell: $"); cputhex8( pixel_x >> 3); cputc('.'); cputhex8( pixel_x & 0x7); cputc(','); cputhex8( pixel_y >> 3); cputc('.'); cputhex8( pixel_y & 0x7);
 
-  // Work out the new cell x,y to the lower-right of the sprite
-  new_cell_x = pixel_x >> 3;
-  new_cell_y = pixel_y >> 3;
-  // If it has changed..
-  if ( new_cell_x != cell_x  ||  new_cell_y != cell_y )
-  {
-    // Blank out the old indicator
-    paint( cell_y, cell_x, 0x20 );
-    // Paint in the new one
-    paint( new_cell_y, new_cell_x, 0xa0 );
-    // ..and remember the cell co-ordinates
-    cell_x = new_cell_x;
-    cell_y = new_cell_y;
-  }
+  memset( COLOR_RAM, COLOR_GREEN, 40*20);
+  // Find the row of cells within the character matrix immediately below the
+  // "feet" of the sprite
+  row_above = pixel_y - 1 >> 3;
+  // ..and the row immediately above the "head" of the sprite
+  row_below = pixel_y + SPRITE_HEIGHT >> 3;
+  center_column = pixel_x + SPRITE_WIDTH/2 >> 3;
+
+  // Show the character matrix cells that will be considered for collision
+
+  // Cells painted LIGHT BLUE are considered as the ceiling for collision
+  // ( cuts a jump short)
+  paint( COLOR_LIGHTBLUE, center_column-1, row_above );
+  paint( COLOR_LIGHTBLUE, center_column,   row_above );
+  paint( COLOR_LIGHTBLUE, center_column+1, row_above );
+  // Cells painted YELLOW are considered as the ground for collision ( arrests
+  // a fall)
+  paint( COLOR_YELLOW, center_column-1, row_below );
+  paint( COLOR_YELLOW, center_column,   row_below );
+  paint( COLOR_YELLOW, center_column+1, row_below );
+  // Cells painted PINK are considered as walls for collision ( prevents
+  // further movement in to the wall).  Note that the sprite is allowed to
+  // overlap with the wall slightly because the sprite is allowed to fall
+  // overlapped with the wall.  It's allowed to fall overlapped with the wall
+  // because the feet of a character are not the full width of the sprite so
+  // the actor would appear to be hovering in the air just prior to falling
+  // otherwise ( cf. Creatures).
+  // The alternative would be to have the actor move through the air while
+  // falling such that the shortest drop in the game nevertheless produced
+  // enough horizontal movement to get the actor clear of the wall ( cf.
+  // Dizzy).  It would be jarring to allow a fall overlapping the wall but to
+  // prevent the actor moving back to overlap the wall again having moved away.
+  paint( COLOR_ORANGE, center_column - 1, row_below - 1 );
+  paint( COLOR_ORANGE, center_column + 1, row_below - 1 );
+  // In theory, "row_below - 1" and " - 2" should be checked to prevent
+  // sideways movement too although the cycles can be saved if the map contains
+  // no crawlspaces 1 or 2 cells in height
 }
 
 
 void init()
 {
-  // Start with the sprite in the top-left corner
-  VIC.spr0_x = 24;
-  VIC.spr0_y = 50;
+  // Start with the sprite close to the top-left corner ( but not too close
+  // otherwise the color highlighting will interfere with the SID!)
+  VIC.spr0_x = SPRITE_X_LEFT + 8;
+  VIC.spr0_y = SPRITE_Y_TOP + 8;
   // Make the sprite shape solid
-  memset( TEST_SHAPE, 0xff, 24/3*21 );
+  memset( TEST_SHAPE, 0xff, SPRITE_WIDTH/8*SPRITE_HEIGHT );
   SHAPE_FOR_SPRITE[0] = (uint16_t)TEST_SHAPE >> 6;
   // Pink
   VIC.spr0_color = COLOR_LIGHTRED;
   // ..and visible
   VIC.spr_ena = ( 1 << 0 );
 
-  clrscr();
+  // Fill the screen with chequerboard glyphs intended to denote free space but
+  // which still show the color highlighting
+  memset( CHAR_MATRIX, 0x66, 40*20);
+
+  // Make a box of solid glyphs to aid with visualisation of collision with the
+  // ground, walls and ceiling
+  {
+    int  i;
+    for ( i = 0; i < 8; i += 1)
+    {
+      place( 0x80+' ', 4,     4+i   );
+      place( 0x80+' ', 4+8-1, 4+i   );
+      place( 0x80+' ', 4+i,   4     );
+      place( 0x80+' ', 4+i,   4+8-1 );
+    }
+  }
+
   render();
 }
 
